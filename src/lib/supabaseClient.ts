94f6d0226627b1a74c87da253c051a6cee95e899
lounes.feniri <lounesfeniri@gmail.com>
2025-05-15 23:28:07 +0200

    [EVO] supabase variables in env file

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hkcpvwbfeybfbdwyjkgd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhrY3B2d2JmZXliZmJkd3lqa2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUwOTY4MDIsImV4cCI6MjA2MDY3MjgwMn0.sXGyE9E8gbQ2x_ztfxbhLe51we-IbgRyaQJ1YJPJ79c';
+// Utiliser les variables d'environnement pour les informations de connexion Supabase
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+// Vérifier que les variables d'environnement sont définies
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('Les variables d\'environnement Supabase ne sont pas définies correctement.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
